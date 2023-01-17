514039f4c4ea4cfff5b5778eaa976316b425df19
SNgere <32767319+SNgere@users.noreply.github.com>
2023-01-18 01:46:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def main():
     # front end elements of the web page 
     html_temp = """ 
     <div style ="background-color:yellow;padding:13px"> 
-    <h1 style ="color:black;text-align:center;">Streamlit diabetes diagnosis ML App</h1> 
+    <h1 style ="color:black;text-align:center;">Diabetes Screening Test</h1> 
     </div> 
     """
       
