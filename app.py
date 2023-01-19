ae35f1e1b7d5f32b8a49fd6f8da7c5b27c1c0cbb
SNgere <32767319+SNgere@users.noreply.github.com>
2023-01-19 14:21:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def main():
     st.markdown(html_temp, unsafe_allow_html = True) 
       
     # following lines create boxes in which user can enter data required to make prediction
-    Pregnancies = st.number_input("Pregnancies", value = int)
+    Pregnancies = st.number_input("Pregnancies", value = 5)
     Glucose = st.number_input("Glucose")
     BloodPressure = st.number_input("BloodPressure")
     SkinThickness = st.number_input("SkinThickness")
