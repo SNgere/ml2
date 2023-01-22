602a278abec8883ac11770fef45d5423725597d2
SNgere <32767319+SNgere@users.noreply.github.com>
2023-01-22 13:03:01 +0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -4,7 +4,7 @@ import pickle
 import streamlit as st
  
 # loading the trained model
-pickle_in = open('predictor.pkl', 'rb') 
+pickle_in = open('model.pkl', 'rb') 
 predictor = pickle.load(pickle_in)
  
 @st.cache()
