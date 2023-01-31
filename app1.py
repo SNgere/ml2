45df0745d224357d36e9550332e39ef7cac306e0
SNgere <32767319+SNgere@users.noreply.github.com>
2023-01-31 12:30:21 +0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -29,25 +29,7 @@ def main():
     # front end elements of the web page 
     html_temp = components.html("""
     <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Diabetes Checker</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
 
-    <!--Fonts-->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@700&display=swap" rel="stylesheet">
-    <!--Fonts-->
-</head>
-<body>
         <div class="description">
             <p class="title">Diabetes Checker</p>
 
@@ -79,8 +61,6 @@ def main():
         <div class="footer">
 
         </div>
-</body>
-</html>
 
     """)
       
